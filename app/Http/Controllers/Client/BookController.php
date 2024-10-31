668fbce2b43d269e50586a939c09891e0b9e6cfd
khuonghapc06329 <khuonghapc06329@fpt.edu.vn>
2024-10-31 22:59:31 +0700

    [LARAVEL] - Chức năng gửi tin nhắn (SMS) khi đặt lịch khám (Client)

--- app/Http/Controllers/Client/BookController.php
+++ app/Http/Controllers/Client/BookController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Client\Booking\BookingRequest; // Import BookingRequest
+use App\Http\Requests\Client\Booking\BookingRequest;
 use App\Models\Book;
-use App\Models\Patient;     // Model cho bảng 'books'
-use App\Models\Specialty;  // Model cho bảng 'specialties'
+use App\Models\Patient;
+use App\Models\Specialty;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use App\Mail\BookingConfirmation;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Mail;
+use Infobip\Api\SmsApi;
+use Infobip\Configuration;
+use Infobip\Model\SmsDestination;
+use Infobip\Model\SmsTextualMessage;
+use Infobip\Model\SmsAdvancedTextualRequest;
+use Illuminate\Support\Facades\Log;
+use Infobip\ApiException;
 
 class BookController extends Controller
 {
@@ -32,13 +40,12 @@ class BookController extends Controller
     }
 
     // Xử lý yêu cầu booking
-    public function handleBooking(BookingRequest $request)  // Sử dụng BookingRequest
-    {
 
+
+    public function handleBooking(BookingRequest $request)
+    {
         $book = new Book();
         $book->book_id = $this->generateUserId();
-
-
         $book->name = $request->name;
         $book->phone = $request->phone;
         $book->email = $request->email;
@@ -49,29 +56,54 @@ class BookController extends Controller
         $book->specialty_id = $request->specialty_id;
         $book->role = $request->role;
 
-
+        
         $specialty = Specialty::where('specialty_id', $request->specialty_id)
             ->where('status', 1)
-            ->get();
-
+            ->get(); 
 
         if (!$specialty) {
             return redirect()->back()->with('error', 'Chuyên khoa không tồn tại hoặc đã bị khóa.');
         }
 
-
-        $book->user_id = Auth::check() ? Auth::user()->user_id : null; // Gán null nếu chưa đăng nhập
-
-
+        $book->user_id = Auth::check() ? Auth::user()->user_id : null;
         $book->save();
 
-
+       
         Mail::to($book->email)->send(new BookingConfirmation($book, $specialty));
 
+        $this->sendSmsConfirmation($book);
 
         return redirect()->back()->with('success', 'Đặt lịch thành công');
     }
 
+    private function sendSmsConfirmation($booking): void
+    {
+       
+        $config = new Configuration(getenv('INFOBIP_API_BASE_URL'), getenv('INFOBIP_API_KEY'));
+        $smsApi = new SmsApi(config: $config); 
+
+       
+        $message = new SmsTextualMessage(
+            from: 'Code', 
+            destinations: [
+                new SmsDestination(to: '84' . ltrim($booking->phone, '0'))  // Đảm bảo đúng định dạng
+            ],
+            text: "Xác nhận đặt lịch khám thành công. Thông tin: {$booking->day} vào lúc {$booking->hour}\nBạn có thể xem chi tiết tại: https://khuonghapc06329.id.vn/."
+        );
+
+        $request = new SmsAdvancedTextualRequest(messages: [$message]);
+
+        try {
+           
+            $smsApi->sendSmsMessage($request);
+        } catch (ApiException $apiException) {
+            
+            Log::error("Không thể gửi SMS: " . $apiException->getMessage());
+        } catch (Exception $e) {
+           
+            Log::error("Lỗi không xác định khi gửi SMS: " . $e->getMessage());
+        }
+    }
 
     protected function generateUserId()
     {
